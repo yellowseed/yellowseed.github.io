abda0144194954224479a84ed29f368c06cc78b5
Erika Logie <erika.logie@gmail.com>
2017-10-26 16:04:17 +0200

    Basic contact form starting to look good

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
-<nav class="navbar navbar-toggleable-xs navbar-light bg-faded">
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
   <a class="navbar-brand" href="#"><img src="/images/logo.svg"></img></a>
   <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">Home</span></a>
       </li>
@@ -21,13 +21,3 @@
 </nav>
 
 </template>
-
-<script>
-export default {
-  name: 'navigation',
-  props: [
-    'items',
-    'brand'
-  ]
-}
-</script>
