ed6a7b00ffa13b8b21993dff41848fbcf23db4ca
Erika Logie <erika.logie@gmail.com>
2017-10-31 11:05:15 +0100

    Footer not working now

--- src/components/FootNote.vue
+++ src/components/FootNote.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-FOOTER BBY
+  <div class="container-fluid footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 mt-5">
+          <h3>Our Mission</h3>
+          <p>To design a more equitable, accessible, and participatory trade system that levels the playing field for global farmers,and builds socially and ecologically resilient communities.</p>
+        <div class="col-md-3 col-sm-6 mt-5">
+          <h3>Learn more</h3>
+        </div>
+        <div class="col-md-3 col-sm-6 mt-5">
+          <p>Support</p>
+          <p>Yellow Seed is a 501(c)(3) project of Social and Environmental Entrepreneurs (SEE), a registered public charity, which provides non-profit status.</p>
+          <p>Your donation is fully tax-deductible.</p>
+        </div>
+        <div class="col-md-3 col-sm-6 mt-5">
+          <p>Subscribe to our newsletter</p>
+          <p>We'll update you on the latest happenings in the Yellow Seed community.</p>
+          <p>
+            <a href='https://www.yellow-seed.org/newsletters'>Newsletter archive »</a>
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1 hLine">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="followUs text-center">Follow us on the web!</p>
+          <p class="text-center" style="padding-top: 10px;"></p>
+          <p class="copyright text-center">&copy; 2017 Yellow Seed</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
