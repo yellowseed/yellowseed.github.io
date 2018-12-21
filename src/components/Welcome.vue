8e950570a9cddc1e426173e0ede69724e24f059d
Wally the Wizard <wizard@17thdimension.com>
2018-12-21 12:00:08 -0700

    subscribe button

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -8,11 +8,33 @@
     <p class='center'>Yellow Seed has closed our beta platform and we are evolving based on all we have learned. Sign up for our email list below to keep updated.
 .</p>
     <div class="mt-4">
-      
-    </div>
+      <label class='home-email-label'>✉<input  v-model='email' placeholder="email" type="email" name="email" id="home-email" required="required"></label>
+     <br/>
+        <button class="btn-default mt-3" v-on:click="subscribe">subscribe</button>
+       </div>
   </div>
 </div>
 
 
 </div>
 </template>
+<script>
+export default {
+  name: 'welcome',
+  data: function () {
+    return {
+      email:'' 
+    }
+  },
+  methods: {
+    subscribe:function () {
+      document.getElementById('mce-FNAME').value=this.email.substring(0,this.email.indexOf('@'))
+      document.getElementById('mce-LNAME').value=' ~ '
+      document.getElementById('mce-EMAIL').value=this.email
+      document.getElementById('mc-embedded-subscribe-form').submit()
+    }
+  }
+};
+
+</script>
+
